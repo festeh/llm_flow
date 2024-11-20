39d4935395b39f091e06e9407ef82b5c31fd59f8
Dima Lipin <dimich3d@ya.ru>
2024-11-20 19:59:02 +0100

    hi

--- cmd/lsp-server/main.go
+++ cmd/lsp-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/festeh/llm_flow/lsp"
@@ -16,6 +17,8 @@ func main() {
 	// }
 	// defer logFile.Close()
 	// log.SetOutput(logFile)
+
+  log.SetTimeFormat(time.StampMilli)
 
 	ctx := context.Background()
 	server := lsp.NewServer(os.Stdout)
